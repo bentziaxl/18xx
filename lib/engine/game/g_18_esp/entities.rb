24a35f45d230c44de266b8ab6aa9aeeded91e742
bena <bena@il.ibm.com>
2022-07-28 11:41:48 +0300

    add privates and impl auction

--- lib/engine/game/g_18_esp/entities.rb
+++ lib/engine/game/g_18_esp/entities.rb
@@ -4,80 +4,53 @@ module Engine
   module Game
     module G18ESP
       module Entities
-        # companies found only in 1st Edition
         COMPANIES = [
           {
-            sym: 'IMC',
-            name: 'Idarado Mining Company',
-            value: 30,
-            revenue: 5,
-            desc: 'Money gained from mine tokens is doubled for the owning Corporation. '\
-                  'If owned by a Corporation, closes on purchase of “6” train, otherwise '\
-                  'closes on purchase of “5” train.',
-            abilities: [{ type: 'close', owner_type: 'corporation', on_phase: '6' }],
-            color: nil,
-          },
-          {
-            sym: 'GJGR',
-            name: 'Grand Junction and Grand River Valley Railway',
-            value: 40,
-            revenue: 10,
-            desc: 'An owning Corporation may upgrade a yellow town to a green city in '\
-                  'additional to its normal tile lay at any time during its turn. This tile '\
-                  'does not need to be reachable by the corporation\'s trains. Action closes '\
-                  'the company or closes on purchase of “5” train.',
-            color: nil,
-          },
-          {
-            sym: 'DNP',
-            name: 'Denver, Northwestern and Pacific Railroad',
+            sym: 'P1',
+            name: 'Ferrocarril de La Habana a Güines',
             value: 50,
-            revenue: 10,
-            desc: 'An owning Corporation may return a station token to its charter to gain '\
-                  'the token cost. The token is placed on the rightmost (most expensive) empty '\
-                  'token slot with money gained corresponding to empty token slot\'s price. The '\
-                  'corporation must always have at least one token on the board. Action closes '\
-                  'the company or closes on purchase of “5” train.',
+            revenue: 20,
+            desc: 'No special abilities.',
             color: nil,
           },
           {
-            sym: 'Toll',
-            name: 'Saguache & San Juan Toll Road Company',
-            value: 60,
-            revenue: 10,
-            desc: 'An owning Corporation receives a $20 discount on the cost of tile lays. Closes '\
-                  'on purchase of “5” train.',
-            color: nil,
-          },
-          {
-            sym: 'LNPW',
-            name: 'Laramie, North Park and Western Railroad',
+            sym: 'P2',
+            name: 'Ferrocarril de Madrid a Aranjuez o El tren de la fresa',
             value: 70,
             revenue: 15,
-            desc: 'When laying track tiles, an owning Corporation may lay an extra yellow tile at '\
-                  'no cost in addition to its normal tile lay. Action closes the company or closes '\
-                  'on purchase of “5” train.',
+            desc: 'Owning Corporation may place a Strawberry token in Aranjuez to add 20 to all routes it runs to this location',
             color: nil,
           },
           {
-            sym: 'DPRT',
-            name: 'Denver Pacific Railway and Telegraph Company',
+            sym: 'P3',
+            name: 'Ferrocarril de Murcia a Cartagena',
             value: 100,
-            revenue: 15,
-            desc: 'The owner immediately receives one share of either Denver Pacific Railroad, '\
-                  'Colorado and Southern Railroad, Kansas Pacific Railway or Colorado Midland Railway. '\
-                  'The railroad receives money equal to the par value when the President’s Certificate '\
-                  'is purchased. Closes on purchase of “5” train.',
+            revenue: 20,
+            desc: 'Comes with 1 Mining Exploitation Authorization (MEA) license. Using the MEA does not close the company.',
             color: nil,
           },
           {
-            sym: 'DRGR',
-            name: 'Denver & Rio Grande Railway Silverton Branch',
+            sym: 'P4',
+            name: 'Ferrocarril de Barcelona a Mataró',
             value: 120,
-            revenue: 25,
-            desc: 'The owner receives the Presidency of Durango and Silverton Narrow Gauge, which '\
-                  'floats immediately. Closes when the DSNG runs a train or on purchase of “5” train. '\
-                  'Cannot be purchased by a Corporation. Does not count towards net worth.',
+            revenue: 20,
+            desc: 'Owning Corporation receives 2H train. The train can not run in the same OR this company is purchased',
+            color: nil,
+          },
+          {
+            sym: 'P5',
+            name: 'Ferrocarril de Alar del Rey a Santander',
+            value: 160,
+            revenue: 15,
+            desc: '10% share of El Ferrocarril de La Robla',
+            color: nil,
+          },
+          {
+            sym: 'P6',
+            name: 'Ferrocarril de Carreño',
+            value: 180,
+            revenue: 30,
+            desc: 'President share of one Northern major company (randomly selected before the game starts). It closes when the major company buys its first train.',
             color: nil,
           },
         ].freeze
