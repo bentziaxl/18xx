a3ebd6e6c306769fd8a918460c93f20c9c696484
bena <bena@il.ibm.com>
2022-08-04 13:49:11 +0300

    fix f trains running

---
+++ lib/engine/game/g_18_esp/corporation.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Engine
+  module Game
+    module G18ESP
+      class Corporation < Engine::Corporation
+        def runnable_trains
+          @trains.reject(&:operated).reject { |train| train.name == 'F' }
+        end
+      end
+    end
+  end
+end
