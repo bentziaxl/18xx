2f0463d3338ed750bbac5c55d81c9a9bb026e9e1
bena <bena@il.ibm.com>
2022-08-08 15:47:09 +0300

    update latest

--- lib/engine/game/g_18_esp/step/special_track.rb
+++ lib/engine/game/g_18_esp/step/special_track.rb
@@ -9,9 +9,17 @@ module Engine
         class SpecialTrack < Engine::Step::SpecialTrack
           def process_lay_tile(action)
             super
+            @round.mea_hex = action.hex
             convert_to_train(action.entity)
             @log << "#{action.entity.name} closes and is converted to a 2H train"
           end
+
+          def round_state
+            super.merge({
+                          mea_hex: nil,
+                        })
+          end
+
 
           def convert_to_train(company)
             @owner = company.owner
