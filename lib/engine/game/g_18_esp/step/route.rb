f5388ff847d184834f74e091aec5028941c00949
bena <bena@il.ibm.com>
2022-08-11 14:30:55 +0300

    discard F train after usage

--- lib/engine/game/g_18_esp/step/route.rb
+++ lib/engine/game/g_18_esp/step/route.rb
@@ -8,7 +8,13 @@ module Engine
       module Step
         class Route < Engine::Step::Route
           def setup
+            puts("here in setup #{@round.mea_hex}")
             super
+          end
+
+          def process_run_routes(action)
+            super
+            @game.discard_f_train(action) if action.entity.trains.any? { |train| train.name == 'F' }
           end
         end
       end
