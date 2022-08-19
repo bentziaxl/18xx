cb467f4724c916eca0e138fa7f7260cb7590c786
bena <bena@il.ibm.com>
2022-08-19 14:11:13 +0300

    remove f train after usage

--- lib/engine/game/g_18_esp/step/dividend.rb
+++ lib/engine/game/g_18_esp/step/dividend.rb
@@ -14,6 +14,15 @@ module Engine
               @log << "#{action.entity.name} retains a subsidy of #{@game.format_currency(subsidy)}"
             end
             super
+            train = action.entity.trains.find { |t| t.name == 'F' }
+            return unless train
+
+            train.operated = false
+            @game.depot.reclaim_train(train)
+            # mea_tile = @round.mea_hex&.tile
+            # mea_tile&.remove_temp_halt
+            # puts(mea_tile&.code)
+            # @round.mea_hex&.lay_downgrade(mea_tile)
           end
         end
       end
