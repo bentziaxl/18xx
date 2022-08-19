2dcbf957043b6c2801b51c59373eeb3966b8fdf3
bena <bena@il.ibm.com>
2022-08-19 14:11:13 +0300

    remove f train after usage

--- lib/engine/game/g_18_esp/step/route.rb
+++ lib/engine/game/g_18_esp/step/route.rb
@@ -7,15 +7,10 @@ module Engine
     module G18ESP
       module Step
         class Route < Engine::Step::Route
-          def setup
-            # puts("here in setup #{@round.mea_hex}")
-            super
-          end
-
-          def process_run_routes(action)
-            super
-            @game.discard_f_train(action) if action.entity.trains.any? { |train| train.name == 'F' }
-          end
+          # def process_run_routes(action)
+          #   super
+          #   @game.discard_f_train(action) if action.entity.trains.any? { |train| train.name == 'F' }
+          # end
         end
       end
     end
