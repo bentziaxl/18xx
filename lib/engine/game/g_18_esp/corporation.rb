d5fd0714ed43e46da71068acbabc7bdb6d6c3079
bena <bena@il.ibm.com>
2022-09-05 10:07:02 +0300

    add destination goal logic

--- lib/engine/game/g_18_esp/corporation.rb
+++ lib/engine/game/g_18_esp/corporation.rb
@@ -4,11 +4,43 @@ module Engine
   module Game
     module G18ESP
       class Corporation < Engine::Corporation
-        attr_reader :destination
+        attr_reader :destination, :goals_reached_counter
+        attr_accessor :destination_connected
 
-        def initialize(sym:, name:, **opts)
+        def initialize(game, sym:, name:, **opts)
+          @game = game
           @destination = opts[:destination]
-          super
+          @goals_reached_counter = 0
+          super(sym: sym, name: name, **opts)
+          @tokens = opts[:tokens].map do |price|
+            token = Token.new(self, price: price)
+            token.used = true unless price.zero?
+            token
+          end
+        end
+
+        def destination_connected?
+          @destination_connected
+        end
+
+        def goal_reached!(type)
+          destination_goal_reached! if type == :destination
+          # give company extra money
+          additional_capital = @par_price.price * @goals_reached_counter
+          @game.bank.spend(additional_capital, self)
+          # give company extra token
+          blocked_token = tokens.find { |token| token.used == true && !token.hex }
+          blocked_token&.used = false
+
+          @game.log << "#{name} reached #{type} goal." \
+                       "#{name} receives #{@game.format_currency(additional_capital)} and an extra token"
+          @destination_connected = true
+        end
+
+        def destination_goal_reached!
+          return if @destination_connected
+
+          @goals_reached_counter += 1
         end
       end
     end
