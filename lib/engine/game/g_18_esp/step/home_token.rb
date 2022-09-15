017ec382ab405b50ac08e9db564a6c967650fa1e
bena <bena@il.ibm.com>
2022-09-15 22:59:30 +0300

    add minor starting in town

---
+++ lib/engine/game/g_18_esp/step/home_token.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_relative '../../../step/home_token'
+
+module Engine
+  module Game
+    module G18ESP
+      module Step
+        class HomeToken < Engine::Step::HomeToken
+          def process_place_token(action)
+            super
+          end
+        end
+      end
+    end
+  end
+end
--- lib/engine/game/g_18_esp/step/token.rb
+++ lib/engine/game/g_18_esp/step/token.rb
@@ -7,6 +7,10 @@ module Engine
     module G18ESP
       module Step
         class Token < Engine::Step::Token
+          def place_token(entity, city, token)
+            token.type = :neutral if @game.mountain_pass_token_hex?(city.hex)
+            super(entity, city, token)
+          end
         end
       end
     end
