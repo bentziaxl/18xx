320ef463da0d193985f2d54b568b569e363e5978
bena <bena@il.ibm.com>
2022-09-21 18:31:05 +0300

    allow unlimited buy from market

--- lib/engine/game/g_18_esp/step/buy_sell_par_shares.rb
+++ lib/engine/game/g_18_esp/step/buy_sell_par_shares.rb
@@ -10,6 +10,14 @@ module Engine
           def get_par_prices(entity, corp)
             @game.par_prices(corp).select { |p| p.price * 2 <= entity.cash }
           end
+
+          def can_gain?(entity, bundle, exchange: false)
+            # Can buy above the share limit if from the share pool
+            return true if bundle.owner == @game.share_pool && @game.num_certs(entity) < @game.cert_limit
+
+            super
+          end
+          
         end
       end
     end
