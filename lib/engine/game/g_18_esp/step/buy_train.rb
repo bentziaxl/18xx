1d04ca8095cb693f2347d5a40d955f9ed8d23734
bena <bena@il.ibm.com>
2022-09-21 15:51:30 +0300

    only one wrong type train

--- lib/engine/game/g_18_esp/step/buy_train.rb
+++ lib/engine/game/g_18_esp/step/buy_train.rb
@@ -12,6 +12,22 @@ module Engine
             # Cannot buy F train
             super.reject { |t| t.name == 'F' }
           end
+
+          def buyable_train_variants(train, entity)
+            trains = super
+            type = @game.north_corp?(entity) ? :broad : :narrow
+
+            trains = trains.reject { |v| type?(v[:name], type) } if owns_type?(entity, type)
+            trains
+          end
+
+          def type?(name, type)
+            (name.include?('+') && type == :narrow) || (!name.include?('+') && type == :broad)
+          end
+
+          def owns_type?(entity, type)
+            entity.trains&.any? { |t| t.track_type == type }
+          end
         end
       end
     end
