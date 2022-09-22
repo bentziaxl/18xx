8cddb21e55c785f5e8c5a66ade3ad3b481d91a9b
bena <bena@il.ibm.com>
2022-09-22 12:12:22 +0300

    fix mea city bug

--- lib/engine/game/g_18_esp/step/dividend.rb
+++ lib/engine/game/g_18_esp/step/dividend.rb
@@ -36,6 +36,8 @@ module Engine
             train.operated = false
             @game.depot.reclaim_train(train)
             mea_tile = @round.mea_hex&.tile
+            return unless mea_tile.only_paths?
+
             mea_tile&.remove_temp_halt
             @round.mea_hex&.lay_downgrade(mea_tile)
           end
