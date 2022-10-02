e4292455ace337f5ab3a2c22e5adaba7a766bca3
bena <bena@il.ibm.com>
2022-10-02 19:35:40 +0300

    fix narrow options when doing mea

---
+++ lib/engine/game/g_18_esp/step/lay_tile_check.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module LayTileCheck
+  def potential_tiles(entity, hex)
+    tiles = selected_tiles(entity, hex).uniq(&:name)
+      .select { |t| @game.upgrades_to?(hex.tile, t) }
+      .reject(&:blocks_lay)
+
+    tiles = tiles.reject { |tile| tile.city_towns.empty? && tile.color != :yellow } if @game.north_corp?(entity)
+    unless @game.north_hex?(hex)
+      tiles = tiles.reject do |tile|
+        tile.paths.any? do |path|
+          path.track == :narrow || path.track == :dual
+        end
+      end
+    end
+    if @game.north_hex?(hex) && @game.north_corp?(entity)
+      tiles = tiles.reject do |tile|
+        tile.paths.any? do |path|
+          path.track == :broad
+        end
+      end
+    end
+    tiles
+  end
+
+  def selected_tiles(entity, hex)
+    colors = potential_tile_colors(entity, hex)
+    @game.tiles.select do |tile|
+      @game.tile_valid_for_phase?(tile, hex: hex,
+                                        phase_color_cache: colors) && tile_valid_for_entity(entity, tile)
+    end
+  end
+
+  def tile_valid_for_entity(entity, tile)
+    narrow_only?(entity) ? tile.paths.any? { |p| p.track == :narrow } : true
+  end
+
+  def narrow_only?(entity)
+    corp = entity.corporation? ? entity : entity.owner
+    @game.north_corp?(corp) && !corp.interchange?
+  end
+end
