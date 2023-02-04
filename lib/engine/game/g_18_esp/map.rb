76af894d33db75a56bf39bf7cf01de8390943884
bena <bena@il.ibm.com>
2023-02-04 19:14:35 +0200

    fix extra comma

--- lib/engine/game/g_18_esp/map.rb
+++ lib/engine/game/g_18_esp/map.rb
@@ -264,7 +264,7 @@ module Engine
             'count' => 1,
             'color' => 'brown',
             'code' =>  'city=revenue:60;city=revenue:60;city=revenue:60;path=a:0,b:_0;path=a:1,b:_0;' \
-                        'path=a:2,b:_1;path=a:4,b:_2;path=a:0,b:_2;label=M',,
+                        'path=a:2,b:_1;path=a:4,b:_2;path=a:0,b:_2;label=M',
           },
           'L85' => {
             'count' => 1,
