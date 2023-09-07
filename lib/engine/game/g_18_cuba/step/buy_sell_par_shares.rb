76f880544e95df2da8244bbbe2f179f564dd42ce
Travis CI <travis@travis-ci.org>
2023-09-07 14:59:52 +0300

    partial commit

---
+++ lib/engine/game/g_18_cuba/step/buy_sell_par_shares.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative '../../../step/buy_sell_par_shares'
+
+module Engine
+  module Game
+    module G18Cuba
+      module Step
+        
+        end
+      end
+    end
+  end
+end
