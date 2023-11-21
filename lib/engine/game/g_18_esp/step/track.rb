e8b35038d208aa794ac973e94ad3121adc51d89c
BenA <BENA@il.ibm.com>
2023-11-21 20:36:47 +0200

    remove token move

--- lib/engine/game/g_18_esp/step/track.rb
+++ lib/engine/game/g_18_esp/step/track.rb
@@ -17,7 +17,7 @@ module Engine
             actions = []
             actions << 'lay_tile' if can_lay_tile?(entity)
             actions << 'choose' if opening_mountain_pass?(entity) && @game.can_build_mountain_pass
-            actions += %w[place_token remove_token] if can_place_token?(entity) || can_move_token?(entity)
+            actions += %w[place_token] if can_place_token?(entity)
             actions << 'pass' if actions.any?
 
             case @state
@@ -35,12 +35,6 @@ module Engine
           end
 
           def process_place_token(action)
-            if @state == :move_token
-              raise GameError, "Can't place token in original spot" if @removed_token == action.city.hex
-
-              action.entity.moved_token = true
-              action.entity.tokens.find { |token| !token.used }.price = move_token_price
-            end
             super
             @game.graph.clear
           end
@@ -51,16 +45,6 @@ module Engine
                 tokened_mountain_pass: nil,
               }
             )
-          end
-
-          def min_token_price(tokens)
-            return super unless @state == :move_token
-
-            move_token_price
-          end
-
-          def move_token_price
-            @game.phase.name.to_i <= 4 ? 40 : 80
           end
 
           def place_token(entity, city, token)
@@ -83,41 +67,6 @@ module Engine
             @log << "#{entity.name} pays the bank #{@game.format_currency(extra_cost)}" if extra_cost.positive?
           end
 
-          def can_move_token?(entity)
-            return false unless entity.corporation?
-
-            !entity.moved_token && entity.tokens.dup.count do |token|
-              token.used && token.hex
-            end > 1 && entity.cash >= move_token_price
-          end
-
-          def process_remove_token(action)
-            @entity = action.entity
-            token = action.city.tokens[action.slot]
-            raise GameError, "Cannot remove #{token.corporation.name} token" unless token.corporation == @entity
-
-            home_token = @entity.tokens.first == token
-            raise GameError, 'Cannot remove home token' if home_token
-
-            token.type = :normal if token.type == :neutral
-            token.remove!
-            @log << "Remove token from #{action.city.hex.name}"
-            @removed_token = action.city.hex
-            @state = :move_token
-          end
-
-          def can_replace_token?(entity, token)
-            return true unless token
-
-            token.corporation == entity
-          end
-
-          def help
-            help_text = ['Once a game a corporation can move a non-home token to another location.']
-            help_text << 'Click on the city with the token to remove, then another city to place the token.'
-            help_text if can_move_token?(current_entity)
-          end
-
           def opening_mountain_pass?(entity)
             entity.type != :minor && !@game.opening_new_mountain_pass(entity).empty?
           end
@@ -129,13 +78,6 @@ module Engine
           def choices
             @game.opening_new_mountain_pass(current_entity)
           end
-
-          # def description
-          #   desc = ''
-          #   desc += 'Lay/Upgrade Track' if actions.include?('lay_tile')
-          #   desc += 'Open Mountain Pass' if actions.include?('choose')
-          #   desc
-          # end
 
           def process_choose(action)
             @game.open_mountain_pass(action.entity, action.choice)
