196fdac62c7f7d5d2f6879f7315f5582dc9d17a6
BenA <BENA@il.ibm.com>
2024-06-09 13:31:56 +0300

    add more rule impl

--- lib/engine/game/g_18_cuba/step/track.rb
+++ lib/engine/game/g_18_cuba/step/track.rb
@@ -2,14 +2,18 @@
 
 require_relative '../../../step/tracker'
 require_relative '../../../step/track'
+require_relative 'skip_fc'
 
 module Engine
   module Game
     module G18Cuba
       module Step
         class Track < Engine::Step::Track
+          include SkipFc
           def potential_tiles(entity, hex)
             tiles = super
+            tiles.reject! { |t| t.cities.sum(&:normal_slots) == 1 } if entity.type == :minor
+
             track_to_reject = entity.type == :minor ? :broad : :narrow
             tiles.reject! do |tile|
               track_types = tile.paths.map(&:track).uniq
