1b1afe2559dc587f45cd6cf76a89be3f3fdb8138
BenA <BENA@il.ibm.com>
2024-06-24 09:55:06 +0300

    update fc revenue

--- lib/engine/game/g_18_cuba/entities.rb
+++ lib/engine/game/g_18_cuba/entities.rb
@@ -327,6 +327,7 @@ module Engine
             city: 1,
             color: :white,
             text_color: :black,
+            always_market_price: true,
             float_percent: 0,
           },
           {
