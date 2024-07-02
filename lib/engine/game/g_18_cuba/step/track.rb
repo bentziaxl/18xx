58fda5504414f2fe929bd5a50e72c11af74f3466
BenA <BENA@il.ibm.com>
2024-07-02 17:57:57 +0300

    add tile laying commissioners

--- lib/engine/game/g_18_cuba/step/track.rb
+++ lib/engine/game/g_18_cuba/step/track.rb
@@ -3,6 +3,7 @@
 require_relative '../../../step/tracker'
 require_relative '../../../step/track'
 require_relative 'skip_fc'
+require_relative 'lay_tile_check'
 
 module Engine
   module Game
@@ -10,44 +11,7 @@ module Engine
       module Step
         class Track < Engine::Step::Track
           include SkipFc
-          def potential_tiles(entity, hex)
-            tiles = super
-            tiles.reject! { |t| t.cities.sum(&:normal_slots) == 1 } if entity.type == :minor
-
-            track_to_reject = entity.type == :minor ? :broad : :narrow
-            tiles.reject! do |tile|
-              track_types = tile.paths.map(&:track).uniq
-              if entity.type == :minor
-                if hex == entity.tokens.first.hex
-                  track_types.size == 1 && track_types.first == track_to_reject
-                else
-                  tile.cities.size > 1 || track_types.first == track_to_reject
-                end
-              else
-                track_types.include?(track_to_reject)
-              end
-            end
-
-            tiles
-          end
-
-          def tracker_available_hex(entity, hex)
-            return nil if entity.type == :minor && !hex.tile.cities.empty? && hex != entity.tokens.first.hex
-
-            connected = hex_neighbors(entity, hex)
-            return nil unless connected
-
-            tile_lay = get_tile_lay(entity)
-            return nil unless tile_lay
-
-            color = hex.tile.color
-            return nil if color == :white && !tile_lay[:lay]
-            return nil if color != :white && !tile_lay[:upgrade]
-            return nil if color != :white && tile_lay[:cannot_reuse_same_hex] && @round.laid_hexes.include?(hex)
-            return nil if ability_blocking_hex(entity, hex)
-
-            connected
-          end
+          include LayTileCheck
 
           def lay_tile(action, extra_cost: 0, entity: nil, spender: nil)
             super
--- lib/engine/step/train.rb
+++ lib/engine/step/train.rb
@@ -238,7 +238,6 @@ module Engine
                 else
                   1
                 end
-          puts("here in issues, #{entity&.id}")
           max = [train.price, buying_power(entity) + entity.owner.cash].min
           [min, max]
         else
