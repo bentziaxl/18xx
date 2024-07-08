1401887b4337cc93648b5abc7a84db3174de3c4e
BenA <BENA@il.ibm.com>
2024-07-08 08:42:26 +0300

    fix assigns of mail and wagon

--- lib/engine/game/g_18_cuba/step/assign.rb
+++ lib/engine/game/g_18_cuba/step/assign.rb
@@ -9,14 +9,16 @@ module Engine
         class Assign < Engine::Step::Assign
           def process_assign(action)
             company = action.entity
-            return super unless assignable_company(company)
+            return super unless company.id == 'M2'
 
             target = action.target
 
             if assignable_corporations(company).include?(target) &&
               (ability = @game.abilities(company, :assign_corporation))
 
-              company.id == 'M2' ? assign_1w(ability, company, target) : assign_1m(ability, company, target)
+              train = @game.depot.upcoming.find { |t| t.name == '1w' }
+              ability.use!
+              train ? gain_1w(company, target, train) : log_no_1w(target)
 
             else
               raise GameError, "Could not assign #{company.name} to #{target.name}; no assignable corporations found"
@@ -28,30 +30,13 @@ module Engine
             company.close!
           end
 
-          def assign_1m(ability, company, target)
-            train = @game.depot.upcoming.find { |t| @game.machine?(t) }
-            ability.use!
-            train ? gain_train(company, target, train) : log_no_train(target)
-          end
-
-          def assign_1w(ability, company, target)
-            train = @game.depot.upcoming.find { |t| t.name == '1w' }
-            ability.use!
-            train ? gain_train(company, target, train) : log_no_train(target)
-          end
-
-          def gain_train(company, target, train)
+          def gain_1w(company, target, train)
             @game.buy_train(target, train, :free)
-            @log << "#{target.name} gains #{train.name} from depot (#{company.name})"
+            @log << "#{target.name} gains 1w from depot (#{company.name})"
           end
 
-          def log_no_train(target)
-            train_name = target.type == :minor ? 'machines' : '1w'
-            @log << "no #{train_name} available in depot. #{target.name} does not gain a train"
-          end
-
-          def assignable_company(entity)
-            entity.id == 'M2' || entity.id == 'M3'
+          def log_no_1w(target)
+            @log << "no 1w available in depot. #{target.name} does not gain a train"
           end
 
           def assignable_corporations(company = nil)
