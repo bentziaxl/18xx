23709aaf01ddd946796a277f8b459ff7f783b9c8
BenA <BENA@il.ibm.com>
2024-07-29 09:01:20 +0300

    add machines logic

--- lib/engine/game/g_18_cuba/step/buy_train.rb
+++ lib/engine/game/g_18_cuba/step/buy_train.rb
@@ -14,13 +14,23 @@ module Engine
             # Cannot buy trains from corps in liquidation.
             track_type = entity.type == :minor ? :narrow : :broad
             trains = super.select { |t| t.track_type == track_type }
-            trains.reject! { |t| @game.extra_train?(t) } unless room_for_extra_trains(entity)
-            trains.select! { |t| game.extra_train(t) } unless room_for_regular_trains(entity)
+
+            if entity.type == :minor
+              trains.reject! { |t| @game.machine?(t) } unless room_for_machines(entity)
+              trains.select! { |t| @game.machine?(t) } unless room_for_regular_trains(entity)
+            else
+              trains.reject! { |t| @game.wagon?(t) } unless room_for_wagons(entity)
+              trains.select! { |t| @game.wagon?(t) } unless room_for_regular_trains(entity)
+            end
             trains
           end
 
-          def room_for_extra_trains(entity)
-            @game.num_extra_train(entity) < @game.train_limit(entity)
+          def room_for_wagons(entity)
+            @game.num_wagons(entity) < @game.train_limit(entity)
+          end
+
+          def room_for_machines(entity)
+            @game.num_machines(entity) < 3
           end
 
           def room_for_regular_trains(entity)
@@ -28,7 +38,8 @@ module Engine
           end
 
           def room?(entity, _shell = nil)
-            room_for_extra_trains(entity) || room_for_regular_trains(entity)
+            room_for_extras = entity.type == :minor ? room_for_machines(entity) : room_for_wagons(entity)
+            room_for_extras || room_for_regular_trains(entity)
           end
         end
       end
